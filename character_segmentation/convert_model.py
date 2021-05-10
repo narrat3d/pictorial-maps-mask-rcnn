051f67da90ae76114980798d194f77a3b6fb5d10
sraimund <sraimund@ethz.ch>
2021-05-10 12:44:43 +0200

    more detailed comment

--- character_segmentation/convert_model.py
+++ character_segmentation/convert_model.py
@@ -39,7 +39,7 @@ for current_step in config.EVAL_STEPS:
                     
                     file_names = os.listdir(output_folder)
                     
-                    # comment if you like to keep intermediate files
+                    # comment if you like to keep intermediate files (e.g. subfolder "saved_model")
                     for file_name in file_names:
                         if (file_name != config.INFERENCE_GRAPH_NAME):
                             file_path = os.path.join(output_folder, file_name)
